fc8b4935b4e31bfb8b388543a6f267c008371c5c
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2025-04-28 13:45:00 +0600

    Update MissingNumberInArray.java

--- src/BinaryProblemSolving/MissingNumberInArray.java
+++ src/BinaryProblemSolving/MissingNumberInArray.java
@@ -14,12 +14,12 @@ public class MissingNumberInArray {
     public static void main(String args[]){
         int arr[] = {1,3,2,4,6};
         
-        int n = arr.length+1;
+        int n = arr.length;
         
         int sum = (n * (n+1)) / 2;
         int currSum = 0;
         
-        for(int i =0; i <arr.length; i++){
+        for(int i =0; i <n; i++){
             currSum+=arr[i];
         }
         
