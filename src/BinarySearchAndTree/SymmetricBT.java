f5539ba62f5445e5702dbaeacad66ab429bb5ccc
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2025-05-04 08:07:49 +0600

    Update SymmetricBT.java

--- src/BinarySearchAndTree/SymmetricBT.java
+++ src/BinarySearchAndTree/SymmetricBT.java
@@ -24,8 +24,13 @@ public class SymmetricBT {
     }
     
     private static boolean isSymmetricUtil(Node root1, Node root2){
-        if(root1 == null || root2 == null){
+
+        if(root1 == null && root2 == null){
             return true;
+        }
+        
+        if(root1 == null || root2 == null){
+            return false;
         }
         
         return ((root1.data == root2.data) 
