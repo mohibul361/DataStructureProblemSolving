208a2b93a20a69e0d21d964b41220c787b7a2c63
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2025-05-08 18:49:45 +0600

    Update DetectACycle.java

--- src/LinkedListProblemSolving/DetectACycle.java
+++ src/LinkedListProblemSolving/DetectACycle.java
@@ -27,7 +27,7 @@ public class DetectACycle {
         Node slow = head;
         Node fast = head;
         
-        while(fast != null && fast.next != null){
+        while(fast != null || fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
             
